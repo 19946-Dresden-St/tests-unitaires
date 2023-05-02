90de7b4bc1872bf8cbb6528ab0e687bf6003bebf
Izïa <izia.crinier@gmail.com>
2023-05-02 22:27:30 +0200

    update user class & tests

--- User.js
+++ User.js
@@ -1,62 +1,76 @@
 class User {
 
   constructor(email, password, firstname, lastname, birthdate) {
-    this.email = email
-    this.password = password
-    this.firstname = firstname
-    this.lastname = lastname
-    this.birthdate = birthdate
+    this.email = email;
+    this.password = password;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthdate = birthdate;
   }
 
-  get email () {
-    return this.email
+  get email() {
+    return this._email;
   }
 
-  set email (value) {
-    this.email = value
+  set email(value) {
+    this._email = value;
   }
 
-  get firstname () {
-    return this.firstname
+  get password() {
+    return this._password;
   }
 
-  set firstname (value) {
-    this.firstname = value
+  set password(value) {
+    this._password = value;
   }
 
-  get password () {
-    return this.password
+  get firstname() {
+    return this._firstname;
   }
 
-  set password (value) {
-    this.password = value
+  set firstname(value) {
+    this._firstname = value;
   }
 
-  get lastname () {
-    return this.lastname
+  get lastname() {
+    return this._lastname;
   }
 
-  set lastname (value) {
-    this.lastname = value
+  set lastname(value) {
+    this._lastname = value;
   }
 
-  get birthdate () {
-    return this.birthdate
+  get birthdate() {
+    return this._birthdate;
   }
 
-  set birthdate (value) {
-    this.birthdate = value
+  set birthdate(value) {
+    this._birthdate = value;
   }
 
-  isValid() {
-    if (this.firstname !== undefined && this.lastname !== undefined) {
-      let today = new Date();
-      console.log(today);
-      let age = today.getFullYear() - this.birthdate.getFullYear();
-      if (age >= 13) {
-        return true
-      }
+  isEmailValid() {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (this.email !== undefined && regex.test(this.email)) {
+      return true;
     }
+  }
+
+  isPasswordValid() {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/;
+    if (this.password !== undefined && regex.test(this.password)) {
+      return true;
+    }
+  }
+
+  isNameValid() {
+    const regex = /^[a-zA-ZÀ-ÿ'\s-]{1,40}$/;
+    if ((this.firstname !== undefined && regex.test(this.firstname)) &&
+       (this.lastname !== undefined && regex.test(this.lastname))) {
+      return true;
+    }
+  }
+
+  isAgeValid() {
   }
 
 }
---
+++ user.test.js
@@ -0,0 +1,20 @@
+const User = require('./User');
+const { describe, test, expect } = require('@jest/globals');
+
+const user = new User('izia.crinier@gmail.com', 'azeAZE123', 'Izïa', 'CRINIER', new Date("1996-04-24T10:27:33.510Z"));
+
+describe("Test de validation d'une adresse mail", () => {
+  test("isEmailValid", () => {
+    expect(user.isEmailValid()).toBe(true);
+  });
+  test("isPasswordValid", () => {
+    expect(user.isPasswordValid()).toBe(true);
+  });
+  test("isNameValid", () => {
+    expect(user.isNameValid()).toBe(true);
+  });
+  test("isAgeValid", () => {
+    expect(user.isAgeValid()).toBe(true);
+  });
+});
+
